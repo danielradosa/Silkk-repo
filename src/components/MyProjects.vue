fb18fc5b50dee7e8c673f4c983c5223f20348065
Daniel Radosa <danysko5@gmail.com>
2022-02-04 22:50:09 +0100

    Restylin projects

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="projects">
-        <div class="text-h3 project-title">Favourite Projects</div>
+        <div class="text-h3 project-title">Favourite Projects <q-icon name="star" color="yellow" /></div>
     </div>
 
-    <div class="project-container">
+      <div class="project-container">
     <div class="q-pa-md project-card">
         <q-card class="my-card single-projects no-shadow no-border-radius">
             <q-img
@@ -43,14 +43,13 @@
         <div class="text-h3 project-title">All Projects</div>
     </div>
 
-    <div class="project-container">
+    <div v-for="(item, index) in items" :key="item.project" class="project-container">
     <div class="q-pa-md project-card">
         <q-card class="my-card single-projects no-shadow no-border-radius">
             <q-img
                 src="https://res.cloudinary.com/practicaldev/image/fetch/s--9yBkqrjS--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/nphrgz8yfnjylrwfr0yl.png">
                 <div class="absolute-bottom">
-                    <div class="text-h6">Damn good project
-                    </div>
+                    <div class="text-h6">Damn good project</div>
                     <div class="text-subtitle2">by Big Boi</div>
                 </div>
             </q-img>
@@ -68,39 +67,7 @@
 
                         <q-card-actions align="center">
                             <q-btn flat no-caps label="Cancel" color="black" class="no-border-radius" v-close-popup />
-                            <q-btn flat no-caps label="Delete" color="red" class="no-border-radius" v-close-popup />
-                        </q-card-actions>
-                    </q-card>
-                </q-dialog>
-
-            </q-card-actions>
-        </q-card>
-    </div>
-    <div class="q-pa-md project-card">
-        <q-card class="my-card single-projects no-shadow no-border-radius">
-            <q-img
-                src="https://res.cloudinary.com/practicaldev/image/fetch/s--9yBkqrjS--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/nphrgz8yfnjylrwfr0yl.png">
-                <div class="absolute-bottom">
-                    <div class="text-h6">Ok project
-                    </div>
-                    <div class="text-subtitle2">by DJ Bowlcut</div>
-                </div>
-            </q-img>
-
-            <q-card-actions class="project-btns">
-                <q-btn unelevated size="md" class="open-btn no-border-radius dropdown-btn">Open</q-btn>
-                <q-btn flat no-caps unelevated size="md" class="delete-btn no-border-radius" color="red"
-                    @click="confirm = true">Delete</q-btn>
-
-                <q-dialog v-model="confirm" persistent class="dialog no-shadow">
-                    <q-card>
-                        <q-card-section class="row items-center">
-                            <span class="q-ml-sm">Are you sure you want to delete this project?</span>
-                        </q-card-section>
-
-                        <q-card-actions align="center">
-                            <q-btn flat no-caps label="Cancel" color="black" class="no-border-radius" v-close-popup />
-                            <q-btn flat no-caps label="Delete" color="red" class="no-border-radius" v-close-popup />
+                            <q-btn @click="this.items.$delete(index, 1)" flat no-caps label="Delete" color="red" class="no-border-radius" v-close-popup />
                         </q-card-actions>
                     </q-card>
                 </q-dialog>
@@ -110,17 +77,21 @@
     </div>
     </div>
 
-</template>
+
+  </template>
 
 <script lang="ts">
     import { ref } from 'vue'
-
     export default {
         name: 'myProjects',
-
         setup() {
             return {
-                confirm: ref(false)
+                confirm: ref(false),
+                items: [
+                    { project: 'one' },
+                    { project: 'two' },
+                    { project: 'three' },
+                ]   
             }
         }
     }
