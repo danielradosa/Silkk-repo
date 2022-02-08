2b9e6f2ae4ea520da192f7ccc9e0e1acce483e03
Daniel Radosa <danysko5@gmail.com>
2022-02-08 14:59:35 +0100

    Update SingleProject.vue

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="projects">
     <div class="text-h3 project-title">Damn good project</div>
-    <div class="text-h5 project-subtitle">created: 13.4.2021</div>
+    <div class="text-h5 project-subtitle">Created: {{allStuff}}</div>
   </div>
 </template>
 
 <script lang="ts">
 
   export default {
+
     setup() {
+      let currentDate = new Date();
+      let day = currentDate.getDate();
+      let month = currentDate.getMonth() + 1;
+      let year = currentDate.getFullYear();
+      let allStuff = `${day}. ${month}. ${year}`
+      
       return {
-        
+       allStuff
       };
     },
+
     name: 'singleProject',
   };
 </script>
