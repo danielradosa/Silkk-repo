4a72f81f4b3ba8b2ac35323934dccaea6348f413
Daniel Radosa <danysko5@gmail.com>
2022-02-19 21:48:33 +0100

    create new styling?

--- src/components/CreateNew.vue
+++ src/components/CreateNew.vue
@@ -3,12 +3,19 @@
     <div class="text-h3 project-title">Create a New Project</div>
     <div class="text-h5 project-subtitle">Fill out form below: <span class="text-red">*</span> All fields are required.</div>
   </div>
-  <div class="q-pa-md">
-    <q-form class="create-form">
-      <q-btn label="Submit" type="submit" color="primary"/>
-      <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+
+    <q-form @submit="onSubmit" 
+            @reset="onReset" 
+            class="q-gutter-md create-form">
+         <q-input filled color="black" v-model="projectTitle" label="Project Title *" lazy-rules :rules="['Please type something']" />
+         <q-input v-model="text" filled autogrow label="Project Description"/>
+         <q-input filled color="black" v-model="projectName" label="Project Name *" lazy-rules :rules="['Please type something']" />
+      <div class="buttons-in">
+        <q-btn label="Back" type="submit" to="/projects" class="dropdown-btn back-btn" unelevated />
+        <q-btn label="Create" type="submit" to="/project" class="outline-btn" outline unelevated />
+      </div>
     </q-form>
-  </div>
+
 </template>
 <script lang="ts">
   export default {
@@ -19,3 +26,17 @@
     name: 'createNewProject',
   };
 </script>
+
+<style lang="scss" scoped>
+.create-form {
+  margin-top: 2em;
+    width: 450px !important;
+    margin-left: 10.5%;
+}
+.back-btn {
+    margin-right: 2em;
+}
+.buttons-in {
+  margin-top: 4em;
+}
+</style>
