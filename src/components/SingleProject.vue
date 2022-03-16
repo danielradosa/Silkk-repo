9268c888a6167d944489dd4207fc31535227a911
Daniel Radosa <danysko5@gmail.com>
2022-03-16 19:11:58 +0100

    Get single project data done
    Now lets figure out how to save and display the data

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,38 +1,67 @@
 <template>
   <div class="projects">
-    <div class="text-h3 project-single-title">{{ projectId }} <q-icon class="edit" name="edit" size=".5em" right></q-icon></div>
-    <div class="desc-contain"></div> 
+    <div class="text-h3 project-single-title">
+      {{ }} Project Title 
+      <q-icon class="edit" name="edit" size=".5em" right></q-icon>
+    </div>
+    <div class="desc-contain"></div>
     <div class="text-h5 project-subtitle">
-      Created: <span> {{}} - {{}}</span>
+      Created: <span> {{}} 12.03.2022 - {{  }} Some Name</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import axios from 'axios';
-import { reactive } from 'vue';
-import { useRoute } from 'vue-router';
 export default {
   setup() {
-    const route = useRoute();
-    const projectId = route.params.id;
-    const state = reactive({
-      projects: {},
-    });
+    type Project = {
+      title: string;
+      author: string;
+      description: string;
+    };
 
-    async function makeGetRequest() {
-      let res = await axios.get('https://sill-api-app.herokuapp.com/api/projects/622b1a17a900330b46af2203');
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      let data = res.data;
-      console.log(data);
+    type GetProjectResponse = {
+      data: Project[];
+    };
+
+    async function getProject() {
+      try {
+        const response = await fetch(
+          'https://sill-api-app.herokuapp.com/api/project/622b1a17a900330b46af2203',
+          {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`Error! status: ${response.status}`);
+        }
+        const result = (await response.json()) as GetProjectResponse;
+        console.log(JSON.stringify(result, null, 4));
+        return result;
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log('error message: ', error.message);
+          return error.message;
+        } else {
+          console.log('unexpected error: ', error);
+          return 'An unexpected error occurred';
+        }
+      }
     }
 
+    void getProject();
+
+    console.log();
+
     return {
-      projectId,
-      state,
-      makeGetRequest
+      getProject,
     };
   },
+
   name: 'singleProject',
 };
 </script>
