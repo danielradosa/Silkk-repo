8d8ff50758ad8bbee6c0c4f03a0514552c230250
suon0001 <suon0001@users.noreply.github.com>
2022-03-29 23:07:19 +0200

    to-do

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -26,6 +26,45 @@
       Description: <span> {{ Project.description }}</span>
     </div>
   </div>
+
+
+<!-- ////////////// TO-DO /////////////////// -->
+<div class="row todo-row">
+  <div style="max-width: 400px">
+    <q-toolbar-title class="text-center">Test</q-toolbar-title>
+    <div>
+    <q-scroll-area style="height: 600px;">
+      <div v-for="n in 7" :key="n" class="q-py-xs">
+     <q-item tag="label" style="max-width: 400px" >
+     <q-separator />
+           
+        <q-item-section class="text-left">
+          <q-item-label class="text-bold" >Notifications</q-item-label> <br>
+          <q-item-label caption>
+            Notify me about updates to apps or games that I downloaded
+          </q-item-label>
+        </q-item-section>
+           <q-item-section side middle>
+          <q-checkbox v-model="check1" />
+          <q-icon class="delete" name="delete" color="grey" />
+        </q-item-section>
+
+      </q-item>
+  
+      </div>
+
+    </q-scroll-area>
+          <q-toolbar  v-ripple>
+   <q-item-section side middle>
+   <q-icon class="add" name="add" color="grey" />
+   </q-item-section>
+   </q-toolbar>
+    </div>
+  
+  </div>
+  <div>First column</div>
+  <div>First column</div>
+</div>
 </template>
 
 <script lang="ts">
