1fb162acddf38582e1f4f4748ed9be4fbd94fc60
Daniel Radosa <danysko5@gmail.com>
2022-04-02 13:26:45 +0200

    f

--- quasar.conf.js
+++ quasar.conf.js
@@ -117,7 +117,7 @@ module.exports = configure(function (ctx) {
 
     // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
-      pwa: false,
+      pwa: true,
 
       // manualStoreHydration: true,
       // manualPostHydrationTrigger: true,
