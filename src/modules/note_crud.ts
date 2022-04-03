c776dfc6ae747a063cf96ce3df55bfbab64c3eeb
Daniel Radosa <danysko5@gmail.com>
2022-04-03 15:04:50 +0200

    small changes for style

---
+++ src/modules/note_crud.ts
@@ -0,0 +1,35 @@
+import { reactive, ref, toRefs } from 'vue'
+import Cookies from 'js-cookie'
+
+const notesCrud = () => {
+    
+    const stateNote = reactive({
+        Note: []
+    })
+
+    const projID = ref(window.location.hash).value.slice(10) || '/';
+
+    const noteTitle = ref('');
+    const noteContent = ref('');
+    
+    type Note = {
+        noteTitle: string,
+        content: string
+    }
+
+    // Create a note
+    
+    // Delete a note
+
+    // Get all notes
+    
+    return {
+        noteTitle,
+        noteContent,
+        ...toRefs(state),
+        projID,
+
+    }
+}
+
+export default notesCrud;
